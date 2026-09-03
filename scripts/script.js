0a319bed5aaa6c8fdb085f10a21930cdc6474926
freddyrios23 <fre.rios@duocuc.cl>
2026-09-02 23:45:30 -0400

    avanze pagina web
    Co-authored-by: Ariel249glitch <ar.jaramillo@duocuc.cl>

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// Catálogo de productos para usar más adelante
+const productos = [ 
+    {
+        id: 1,
+        nombre: "Zapatillas adidas superstar",
+        precio: 79000,
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6TeoFheP4BGfyLSSe1ZDVhHRqFVjkvhZdLv0Xq0-rEQ&s=10"
+    },
+    {
+        id: 2,
+        nombre: "Zapatillas Nike air max plus",
+        precio: 80000,
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUMpQvM-y-Y0QuJ_7c4rkuydoJJskdULLsOb54RppDvQ&s=10"
+    },
+    {
+        id: 3,
+        nombre: "Zapatillas reebok classic",
+        precio: 50000,
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTdKGUozDyIZcmJYHySbA__N4PZWRR_s3rG7kLI0AYHw&s=10"
+    }
+];
